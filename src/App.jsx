f9a9b1a9ab49d01ed1f1c1e19bf4079d6ee31f54
Alejo Janin <alejojanin@gmail.com>
2022-09-22 09:09:24 -0300

    Add Spinner loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Notifications from './components/Notifications'
 import fetchApi from './services/fetchApi'
+import Spinner from './components/Spinner'
 
 const App = () => {
   const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    fetchApi().then((data) => setUsers(data.users))
+    fetchApi()
+      .then((data) => setUsers(data.users))
+      .then(() => setIsLoading(false))
   }, [])
 
   console.log(users)
 
   return (
-    <main className='flex flex-col justify-center items-center h-screen max-w-sm md:max-w-3xl mx-auto p-4 border-2 rounded-lg'>
+    <div className='flex flex-col justify-start items-center h-[90vh] max-w-sm md:max-w-3xl mx-auto p-4 border-2 rounded-lg bg-white'>
       <Header />
-      <Notifications users={users} />
-    </main>
+      {isLoading ? <Spinner /> : <Notifications users={users} />}
+    </div>
   )
 }
 
