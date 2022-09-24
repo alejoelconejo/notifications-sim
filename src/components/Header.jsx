e83455c18c9bc6458c8f90f082976684d95121f8
Alejo Janin <alejojanin@gmail.com>
2022-09-24 10:07:27 -0300

    Change header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,8 @@ import Clock from './Clock'
 
 const Header = () => {
   return (
-    <header className='grid grid-cols-2 md:grid-cols-3 w-full mb-6'>
-      <h2 className='font-extrabold text-xl flex items-center justify-self-start'>
-        Notifications
-        <span className='text-white text-lg rounded-md bg-blue-900 px-3 mx-2'>
-          3
-        </span>
-      </h2>
+    <header className='flex justify-end items-center w-full mb-4'>
       <Clock />
-      <a className='text-gray-500 text-sm justify-self-end flex items-center'>
-        Mark all as read
-      </a>
     </header>
   )
 }
