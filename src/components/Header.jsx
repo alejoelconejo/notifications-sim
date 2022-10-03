1e73d0027a74a3363870496b39e426508a1bac63
Alejo Janin <alejojanin@gmail.com>
2022-10-03 07:16:50 -0300

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import Clock from './Clock'
+import { ReactComponent as Logo } from '../assets/images/logo.svg'
 
 const Header = () => {
   return (
-    <header className='flex justify-end items-center w-full mb-4'>
+    <header className='flex justify-between items-center w-full mb-4'>
+      <a href='/' className='flex gap-2 items-baseline'>
+        <Logo className='w-8 h-8' />
+        <span className='text-xl'>Notifier App</span>
+      </a>
       <Clock />
     </header>
   )
