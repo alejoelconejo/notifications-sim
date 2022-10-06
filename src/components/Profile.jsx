34e0b471af2f86ebb598ab75eb03ab52a156c67a
Alejo Janin <alejojanin@gmail.com>
2022-10-06 08:00:40 -0300

    Add styles to profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,14 +5,32 @@ const Profile = ({ users, params }) => {
 
   const selectedProfile = users.filter((user) => user.id === paramsId)[0]
 
-  console.log(selectedProfile)
+  function getAge(birthDate) {
+    const yearNow = new Date().getFullYear()
+    const yearBirth = new Date(birthDate).getFullYear()
+    return yearNow - yearBirth
+  }
+
   return (
-    <section>
-      <article>
-        <img className='h-28' src={selectedProfile.image}></img>
-        <h2>{selectedProfile.username}</h2>
+    <section className='flex flex-col items-center gap-6'>
+      <article className='flex items-center gap-8 border-4 p-4 rounded-lg mt-8 border-blue-100'>
+        <img
+          className='h-28 border-2 rounded-full '
+          src={selectedProfile.image}
+        ></img>
+        <div>
+          <p className='font-extrabold text-xl'>
+            {selectedProfile.firstName} {selectedProfile.lastName}
+          </p>
+          <p className='text-gray-400 mb-2'>@{selectedProfile.username}</p>
+          <p>Age: {getAge(selectedProfile.birthDate)}</p>
+          <p>Gender: {selectedProfile.gender}</p>
+          <p>E-mail: {selectedProfile.email}</p>
+        </div>
       </article>
-      <Link href='/'>Volver a notificaciones</Link>
+      <Link className='text-gray-600' href='/'>
+        Volver a notificaciones
+      </Link>
     </section>
   )
 }
