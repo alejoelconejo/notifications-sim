9576b0df386f98cf58141d07ed687471d7b16d21
Alejo Janin <alejojanin@gmail.com>
2022-10-07 07:28:56 -0300

    Fix logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import Clock from './Clock'
 import { ReactComponent as Logo } from '../assets/images/logo.svg'
+import { Link } from 'wouter'
 
 const Header = () => {
   return (
     <header className='flex justify-between items-center w-full mb-4'>
-      <a href='/' className='flex gap-2 items-baseline'>
+      <Link href='/' className='flex gap-2 items-baseline'>
         <Logo className='w-8 h-8' />
         <span className='text-xl'>Notifier App</span>
-      </a>
+      </Link>
       <Clock />
     </header>
   )
